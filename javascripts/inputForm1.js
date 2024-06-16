9561c9ff8c2269f19a62ca1d0a2055ca0185d069
mmccjjj <d.o.huber88@gmail.com>
2024-06-16 22:42:49 +0200

    ..

--- javascripts/inputForm1.js
+++ javascripts/inputForm1.js
@@ -1,7 +1,7 @@
 let text= document.getElementById("test");
-let languages= localStorage.getItem("language");
+let language= localStorage.getItem("language");
 let level= localStorage.getItem("level");
 let date= localStorage.getItem("date")
 let time= localStorage.getItem("time")
 
-text.innerText= "You would like to book the " + level+ " "+ languages+ " course on the "+ date+ " at "+ time
+text.innerText= `You would like to book the ${level} ${language} course on the ${date} at ${time}` 
