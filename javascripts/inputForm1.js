8c5f9b0a0015e1b181d402598b9ac995b3b4df4a
mmccjjj <dhuber88@sunrise.ch>
2024-06-18 11:21:41 +0200

    funktionalität steht

--- javascripts/inputForm1.js
+++ javascripts/inputForm1.js
@@ -3,6 +3,11 @@ let language= localStorage.getItem("language");
 let level= localStorage.getItem("level");
 let date= localStorage.getItem("date");
 let time= localStorage.getItem("time");
+let inEmail= document.getElementById("email");
+let inUsername= document.getElementById("username");
+let inPassword= document.getElementById("password");
+let inRePassword= document.getElementById("re-password");
+let nextButton= document.getElementById("nextButton");
 
 if (!language || !level || !date || !time){
 
@@ -23,20 +28,27 @@ const password2= document.getElementById("re-password")
 password1.addEventListener("input", validatePassword);
 password2.addEventListener("input", validatePassword);
 
-function validatePassword(){
-    const pw1Value = password1.value;
-    const pw2Value = password2.value;
+function validatePassword() {
+    let pw1Value = password1.value;
+    let pw2Value = password2.value;
 
-    if (pw1Value === pw2Value) {
-        // Passwörter stimmen überein, entferne die Klasse "wrong" von beiden Input-Feldern
-        password1.classList.remove("wrong");
-        password2.classList.remove("wrong");
-    } else {
-        // Passwörter stimmen nicht überein, füge die Klasse "wrong" zu beiden Input-Feldern hinzu
-        password1.classList.add("wrong");
-        password2.classList.add("wrong");
-    }
-}
+    if (pw1Value.length >= 6 && pw2Value.length >= 6){
+            // Überprüfung auf Gleichheit der Passwörter
+            if (pw1Value === pw2Value) {
+                password1.classList.remove("wrong");
+                password2.classList.remove("wrong");
+            } else {
+                password1.classList.add("wrong");
+                password2.classList.add("wrong");
+               }
+
+            }else {
+                // Markierung hinzufügen, wenn Passwort zu kurz ist
+                inPassword.classList.add("wrong");
+                inRePassword.classList.add("wrong");
+            }
+        }
+
 
 
 function toggleElements() {
@@ -45,3 +57,97 @@ function toggleElements() {
         elements[i].classList.toggle("hideActiv");
     }
 }
+
+
+function goBack() {
+    window.history.back();
+}
+
+function validateEmail(email) {
+
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const emailInput = document.getElementById('email');
+
+    emailInput.addEventListener("input", function() {
+    validateForm(emailInput);
+    });
+
+    function validateForm(input) {
+        if (validateEmail(input.value)) {
+                input.classList.remove("wrong");
+     } else {
+                input.classList.add("wrong");
+            }
+            }
+    });
+
+    inUsername.addEventListener("input",checkUsername)
+
+    function checkUsername(){
+        let usernameValid= inUsername.value;
+
+        if(usernameValid.length< 3){
+            inUsername.classList.add("wrong");
+        } else inUsername.classList.remove("wrong");
+    }
+
+    nextButton.addEventListener("click", function(event) {
+    
+        if (nextButtonFunction()) {
+            window.location.href = "inputForm2.html"; 
+        }else{
+                alert("At least one field was not filled out correctly.");
+        }
+    });
+    function nextButtonFunction() {
+        let emailValid = validateEmail(inEmail.value);
+        let pw1Value = inPassword.value;
+        let pw2Value = inRePassword.value;
+        let usernameValid= inUsername.value;
+
+        if(!usernameValid || usernameValid.length< 3){
+            inUsername.classList.add("wrong");
+        } else inUsername.classList.remove("wrong");
+    
+        // Validierung der E-Mail-Adresse und Markierung des E-Mail-Felds
+        if (!emailValid) {
+            inEmail.classList.add("wrong");
+        } else {
+            inEmail.classList.remove("wrong");
+        }
+    
+        // Validierung der Passwörter und Markierung der Passwort-Felder
+        // Überprüfung, ob Passwortfelder leer sind
+        if (pw1Value.length >= 6 && pw2Value.length >= 6) {
+            // Überprüfung auf Gleichheit der Passwörter
+            if (pw1Value === pw2Value) {
+                inPassword.classList.remove("wrong");
+                inRePassword.classList.remove("wrong");
+            } else {
+                inPassword.classList.add("wrong");
+                inRePassword.classList.add("wrong");
+            }}
+        else {
+                // Markierung hinzufügen, wenn Passwort zu kurz ist
+                inPassword.classList.add("wrong");
+                inRePassword.classList.add("wrong");
+            }
+
+    
+    
+        // Speichern der Daten im LocalStorage oder Alert auslösen
+        if (emailValid && (pw1Value === pw2Value) && (pw1Value.trim() !== '' && pw2Value.trim() !== '')) {
+            localStorage.setItem("username", inUsername.value);
+            localStorage.setItem("email", inEmail.value);
+        } else {
+            alert("At least one field was not filled out correctly.");
+        }
+        return emailValid && (pw1Value === pw2Value) && (pw1Value.length >= 6 && pw2Value.length >= 6) && (usernameValid && usernameValid.length >= 3);
+    }
+    
+
